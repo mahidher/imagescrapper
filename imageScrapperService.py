9aee565646728b17d18a854648eda49fbfe670e0
mahidher <dkmahidher@gmail.com>
2020-08-03 00:01:50 +0530

    changed driver options

--- imageScrapperService.py
+++ imageScrapperService.py
@@ -12,8 +12,10 @@ class imageScrapperService:
         imageObj = ImageUtils()
         #driver = webdriver.Chrome('./chromedriver')
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+
         chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
         driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options)
         driver.get(url)
